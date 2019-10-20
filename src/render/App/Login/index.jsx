4f1af0ab48096012f22688410d18f2128dd9450b
tanjinjie <a@PeterdeMacBook-Pro.local>
2019-10-20 18:28:58 +0800

    修复了文件

--- src/render/App/Login/index.jsx
+++ src/render/App/Login/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import './index.less';
 import { connect } from 'dva';
+import { login, authentication } from '../../utills/API/index';
+import sha1 from 'js-sha1';
+import './index.less';
 class App extends React.PureComponent {
   state = {
     isShowAnimation: true,
@@ -20,6 +22,30 @@ class App extends React.PureComponent {
       clearInterval(this.timer);
     }, 6000);
   }
+
+  handleLogin = async () => {
+    const result = await login({ username: this.username, password: sha1(this.password) });
+    if (result.code === 0 || result.code === 1) {
+      this.username = '';
+      this.password = '';
+      alert('登陆成功');
+      this.props.history.replace('/video');
+    } else {
+      alert('请求失败');
+    }
+  };
+
+  handleChange = (tag, e) => {
+    switch (tag) {
+      case 'username':
+        this.username = e.target.value;
+        break;
+      case 'password':
+        this.password = e.target.value;
+        break;
+    }
+  };
+
   render() {
     const { isShowAnimation, name, isShowLogin } = this.state;
     return isShowAnimation ? (
@@ -28,10 +54,15 @@ class App extends React.PureComponent {
       <div
         className="login-css "
         style={{ WebkitAppRegion: 'drag' }}
-        onClick={() => {
+        onClick={async () => {
           this.setState({
             isShowLogin: true,
           });
+          const result = await authentication();
+          if (result.code === 0) {
+            alert('免登陆');
+            this.props.history.replace('/video');
+          }
         }}
       ></div>
     ) : (
@@ -39,19 +70,13 @@ class App extends React.PureComponent {
         <div className="login-content">
           <div>
             <label htmlFor="text">账号:</label>
-            <input type="text" />
+            <input type="text" onChange={this.handleChange.bind(this, 'username')} />
           </div>
           <div>
             <label htmlFor="text">密码:</label>
-            <input type="password" />
+            <input type="password" onChange={this.handleChange.bind(this, 'password')} />
           </div>
-          <button
-            className="login-button"
-            onClick={() => {
-              console.log('登陆');
-              this.props.history.replace('/video');
-            }}
-          >
+          <button className="login-button" onClick={this.handleLogin}>
             登陆
           </button>
           <button className="login-button">注册</button>
