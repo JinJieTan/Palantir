8d4d22fb2b33fef63f1bccb564c0cf164d374d2e
tanjinjie <a@PeterdeMacBook-Pro.local>
2019-10-23 19:03:39 +0800

    窗口路由跳转

--- src/render/App/Login/index.jsx
+++ src/render/App/Login/index.jsx
@@ -1,37 +1,32 @@
 import React from 'react';
 import { connect } from 'dva';
+import { ipcRenderer } from 'electron';
 import { login, authentication } from '../../utills/API/index';
 import sha1 from 'js-sha1';
 import './index.less';
 class App extends React.PureComponent {
-  state = {
-    isShowAnimation: true,
-    name: 1,
-    isShowLogin: false,
-    imgSrc:require('../assets/image/4.jpeg'),
-  };
-  componentDidMount() {
-    this.timer = setInterval(() => {
-      this.setState({
-        name: this.state.name + 1,
-      });
-    }, 2000);
-    setTimeout(() => {
-      this.setState({
-        isShowAnimation: false,
-      });
-      clearInterval(this.timer);
-    }, 6000);
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowAnimation: true,
+      name: 1,
+      isShowLogin: false,
+      imgSrc: require('../assets/image/4.jpeg'),
+    };
+    if (localStorage.getItem('username')) {
+      props.history.replace('/home');
+      localStorage.removeItem('username');
+    }
   }
-
   handleLogin = async () => {
     //点击发送此时应该鉴权 已经保存在内存中 通过this.username 和 this.password 获取～
     const result = await login({ username: this.username, password: sha1(this.password) });
     if (result.code === 0 || result.code === 1) {
+      alert(result.data);
+      localStorage.setItem('username', this.username);
+      ipcRenderer.send('Enter-Home', { width: 800, height: 600 });
       this.username = '';
       this.password = '';
-      alert(result.data);
-      this.props.history.replace('/video');
     } else {
       alert(result.data);
       return;
@@ -50,59 +45,44 @@ class App extends React.PureComponent {
   };
 
   render() {
-    const { isShowAnimation, name, isShowLogin ,imgSrc } = this.state;
-    return isShowAnimation ? (
-      <div className={('login-animation', `animation${name}`)}></div>
-    ) : !isShowLogin ? (
-      <div
-        className="login-css "
-        style={{ WebkitAppRegion: 'drag' }}
-        onClick={async () => {
-          this.setState({
-            isShowLogin: true,
-          });
-          const result = await authentication();
-          if (result.code === 0) {
-            alert('免登陆');
-            this.props.history.replace('/video');
-          }
-        }}
-      ></div>
-    ) : (
-          <div className="login-container">
-            <div className="login-content">
-              <div>
-                <label htmlFor="text" className="login-title" >Palantir</label>
-              </div>
-              <div>
-                <img src={imgSrc} />
-              </div>
-              <div>
-                {/* <label htmlFor="text">账号:</label> */}
-                <input type="text" placeholder='请输入账号' onChange={this.handleChange.bind(this, 'username')} />
-              </div>
-              <div>
-                {/* <label htmlFor="text">密码:</label> */}
-                <input type="password" placeholder='请输入密码' onChange={this.handleChange.bind(this, 'password')} />
-              </div>
-              <div>
-                <label>
-                  <input type='radio' name='1' />
-                  <span>记住密码</span>
-                </label>
-                <label>
-                  <input type='radio' name='1' />
-                  <span>自动登录</span>
-                </label>
-              </div>
-              <button className="login-button" onClick={this.handleLogin}>
-                登陆
-              </button>
-              <button className="login-button">自动注册</button>
-              <a href='javascript:void(0)'>忘记密码</a>
-            </div>
+    const { imgSrc } = this.state;
+    return (
+      <div className="login-container">
+        <div className="login-content">
+          <div>
+            <label htmlFor="text" className="login-title">
+              Palantir
+            </label>
           </div>
-        );
+          <div>
+            <img src={imgSrc} />
+          </div>
+          <div>
+            {/* <label htmlFor="text">账号:</label> */}
+            <input type="text" placeholder="请输入账号" onChange={this.handleChange.bind(this, 'username')} />
+          </div>
+          <div>
+            {/* <label htmlFor="text">密码:</label> */}
+            <input type="password" placeholder="请输入密码" onChange={this.handleChange.bind(this, 'password')} />
+          </div>
+          <div>
+            <label>
+              <input type="radio" name="1" />
+              <span>记住密码</span>
+            </label>
+            <label>
+              <input type="radio" name="1" />
+              <span>自动登录</span>
+            </label>
+          </div>
+          <button className="login-button" onClick={this.handleLogin}>
+            登陆
+          </button>
+          <button className="login-button">自动注册</button>
+          <a href="javascript:void(0)">忘记密码</a>
+        </div>
+      </div>
+    );
   }
 }
 
