0d69c9fa41439b1428b5f6f0629b3a1c8d94d3ae
tanjinjie <a@PeterdeMacBook-Pro.local>
2019-10-25 17:04:50 +0800

    修改了登陆和主页的尺寸

--- src/main/main.js
+++ src/main/main.js
@@ -50,7 +50,7 @@ function createWindow(width, height, event) {
 }
 
 app.on('ready', () => {
-  createWindow(400, 460);
+  createWindow(300, 420);
 });
 
 // Quit when all windows are closed.
@@ -61,7 +61,7 @@ app.on('window-all-closed', function() {
 app.on('activate', function() {
   if (mainWindow === null) {
     () => {
-      createWindow(342, 417);
+      createWindow(300, 420);
     };
   }
 });
