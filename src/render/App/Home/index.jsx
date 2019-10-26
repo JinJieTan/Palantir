db44377213ec960728cf967ed20b90f6ae3bad2a
10.12 <2251577986@qq.cm>
2019-10-26 14:23:26 +0800

    添加分类页样式

--- src/render/App/Home/index.jsx
+++ src/render/App/Home/index.jsx
@@ -14,38 +14,70 @@ export default class App extends React.PureComponent {
     };
   }
   render() {
+    let liveScan = (length) => {
+
+      var res = [];
+
+      for (var i = 0; i < length; i++) {
+
+          res.push(<div className="liveScan">
+            <img src="" className="scanImg"/>
+            <p>视频标题</p>
+            <div>
+              <div className="">
+                <img src="" className="icon"/>
+                99999
+              </div>
+              <div>
+                <img src="" className="icon"/>
+                99999
+              </div>
+            </div>
+          </div>)
+
+      }
+
+      return res
+
+  }
     return (
       <div className="Main">
         <div className="container">
           <nav>
+            <img src={ this.state.starsImg } className="classify"/>
             <ul>
               <li >
                 <img src={ this.state.starsImg } className="stars"/>
                 首页
               </li>
+              <li >
+                <img src={ this.state.starsImg } className="stars"/>
+                生活
+              </li>
             </ul>
           </nav>
           <main>
-          <h1>Home</h1>
-        <button
-          onClick={async () => {
-            const isNoPassword = window.confirm('是否选择退出免密码登陆？');
-            if (isNoPassword) {
-              const result = await clearSession();
-              if (result.code === 0) {
-                alert('退出登陆成功');
-              }
-            }
-            ipcRenderer.send('Enter-Home', { width: 342, height: 417 });
-          }}
-        >
-          返回登陆
-        </button>
+            { liveScan(20) }
+            {/* <h1>Home</h1>
+            <button
+              onClick={async () => {
+                const isNoPassword = window.confirm('是否选择退出免密码登陆？');
+                if (isNoPassword) {
+                  const result = await clearSession();
+                  if (result.code === 0) {
+                    alert('退出登陆成功');
+                  }
+                }
+                ipcRenderer.send('Enter-Home', { width: 342, height: 417 });
+              }}
+            >
+              返回登陆
+            </button> */}
           </main>
         </div>
-        <footer>
+        {/* <footer>
           底部
-        </footer>
+        </footer> */}
 
       </div>
     );
