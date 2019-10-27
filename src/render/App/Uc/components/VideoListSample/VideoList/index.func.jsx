83bf1f8f8a2ec16391c4eb6bc97331048b88620b
Lynch <lynch201@163.com>
2019-10-27 22:02:50 +0800

    “我的”模块新增静态页：动态、3个相同的视频页

---
+++ src/render/App/Uc/components/VideoListSample/VideoList/index.func.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.less';
+import postDef from '../../../../assets/image/1.jpeg';
+
+export default function VideoList(props) {
+  const { items } = props;
+
+  return (
+    <ul className="videolist-container clearfix">
+      {items.map((e, i) => {
+        const { id, name, imgUrl, url } = e;
+        return (
+          <li key={id}>
+            <Link to={url}>
+              <div className="videolist-item-img-container">
+                <img src={imgUrl ? imgUrl : postDef} alt=""/>
+              </div>
+              <p className="videolist-item-name">{name}</p>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
