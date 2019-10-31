d7291aac9b49bb6f46e7d9e8abb0eea7a5aaa89d
tanjinjie <a@PeterdeMacBook-Pro.local>
2019-10-31 09:55:49 +0800

    暂存

---
+++ src/render/App/Uc/startVue.js
@@ -0,0 +1,20 @@
+import Vue from 'vue/dist/vue.common.js';
+// import routers from 'vue-router';
+import app from './xxxx.vue';
+function test(props) {
+  console.log(props);
+  // new Vue({
+  //   el: '#test',
+  //   data: {
+  //     msg: 'Hello, Vue.js.',
+  //   },
+  //   template: '<div></div>',
+  //   render: (h) => h('h1', {}, 'Hi, Vue.js '),
+  // });
+  new Vue({
+    el: '#test',
+    render: (h) => h(App),
+  });
+}
+
+export default test;
