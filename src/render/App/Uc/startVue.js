4afff1877eb88e04b0bf68892095c17068349d7e
tanjinjie <a@PeterdeMacBook-Pro.local>
2019-10-31 11:01:31 +0800

    接入vue，等待调试

--- src/render/App/Uc/startVue.js
+++ src/render/App/Uc/startVue.js
@@ -1,22 +1,29 @@
 import Vue from 'vue/dist/vue.common.js';
 // import routers from 'vue-router';
-import app from './xxxx.vue';
+import App from './app.vue';
+Vue.config.productionTip = false;
 function test(props) {
   console.log(props);
-  new Vue({
-    el: '#test',
-    data: {
-      msg: 'Hello, Vue.js.',
-    },
-    template: '<div></div>',
-    render: (h) => h('h1', {}, 'Hi, Vue.js '),
-  });
   // new Vue({
-  //   el: 'test',
-  //   render(h) {
-  //     return h(app);
+  //   el: '#test',
+  //   data: {
+  //     msg: 'Hello, Vue.js.',
   //   },
+  //   template: '<div></div>',
+  //   render: (h) => h('h1', {}, 'Hi, Vue.js '),
   // });
+  var Profile = Vue.extend({
+    template: '<p>{{firstName}} {{lastName}} aka {{alias}}</p>',
+    data: function() {
+      return {
+        firstName: 'Walter',
+        lastName: 'White',
+        alias: 'Heisenberg',
+      };
+    },
+  });
+  // 创建 Profile 实例，并挂载到一个元素上。
+  new Profile().$mount('#test');
 }
 
 export default test;
