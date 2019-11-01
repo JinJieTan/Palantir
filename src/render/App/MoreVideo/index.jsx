9431925f910f79e7dabfcdd1085f7cd43f1e01f0
weblixin <weblixin@163.com>
2019-11-01 16:39:33 +0800

    首页查看更多--视频列表模块

---
+++ src/render/App/MoreVideo/index.jsx
@@ -0,0 +1,47 @@
+import React, {PureComponent} from 'react';
+
+import './index.less';
+
+const imgSrc = require("../assets/image/4.jpeg")
+
+class MoreVideo extends PureComponent {
+
+  componentWillMount() {
+    window.scrollTo(0, 0)
+  }
+
+  render() {
+    let liveScan = (length) => {
+
+      var res = [];
+
+      for (var i = 0; i < length; i++) {
+
+          res.push(<div className="liveScan">
+            <img src={imgSrc} className="scanImg"/>
+            <p className='title'>视频标题</p>
+            <div>
+              <div className="">
+                <img src={imgSrc} className="icon"/>
+                99999
+              </div>
+              <div>
+                <img src={imgSrc} className="icon"/>
+                99999
+              </div>
+            </div>
+          </div>)
+
+      }
+
+      return res
+    }
+    return (
+      <div className="more-video">
+        { liveScan(20) }
+      </div>
+    )
+  }
+};
+
+export default MoreVideo;
